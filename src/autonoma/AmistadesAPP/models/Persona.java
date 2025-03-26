a1bf95a82d2ee7f78a4948dbdc9c8b99c926e290
Juan Manuel Ochoa <juanm.ochoamu@autonoma.edu.co>
2025-03-26 10:22:30 -0500

    Actualizacion persona

--- src/autonoma/AmistadesAPP/models/Persona.java
+++ src/autonoma/AmistadesAPP/models/Persona.java
@@ -15,11 +15,23 @@ public abstract class Persona {
         return nombre;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public String getTelefono() {
         return telefono;
     }
 
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
     public String getCorreo() {
         return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
     }
 }
